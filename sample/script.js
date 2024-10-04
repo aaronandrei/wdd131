aaa19b7104931aafa5e61440373088a8eb4e7e26
aaronandrei <aaronandreim@gmail.com>
2024-10-03 22:25:20 -0600

    chnes

--- sample/script.js
+++ sample/script.js
@@ -9,3 +9,5 @@ const buttons = document.querySelectorAll("button");
 for (const button of buttons) {
     button.addEventListener("click", createParagraph);
 }
+(document.lastModified);
+let oLastModif = Date(document.lastModified);
